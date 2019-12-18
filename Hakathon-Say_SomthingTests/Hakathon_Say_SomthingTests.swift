cbf23683a9fdb74efd2d8f04bf5e1cb0fc89e384
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-12-18 12:51:16 -0500

    Initial commit

---
+++ Hakathon-Say_SomthingTests/Hakathon_Say_SomthingTests.swift
@@ -0,0 +1,34 @@
+//
+//  Hakathon_Say_SomthingTests.swift
+//  Hakathon-Say_SomthingTests
+//
+//  Created by Mr Wonderful on 12/18/19.
+//  Copyright © 2019 Mr Wonderful. All rights reserved.
+//
+
+import XCTest
+@testable import Hakathon_Say_Somthing
+
+class Hakathon_Say_SomthingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
